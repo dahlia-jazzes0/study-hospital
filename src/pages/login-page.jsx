4a0b77e04e21c87a235b4cc7c1e8df020ccee08e
hyeon <nguswjd02@gmail.com>
2025-07-04 18:24:16 +0900

    로그인페이지 css 구현

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -1,44 +1,46 @@
+import './login-page.css';
+
 export function LoginPage() {
   return (
     <>
-      <header>
-        <h1>
-          <img
-            src="logo.png"
-            alt="모두한의원 로고"
-            style={{ width: '600px' }}
-          />
+      <div className="loginPageHeader">
+        <h1 className="logo">
+          <img src="logo.png" alt="모두한의원 로고" />
         </h1>
 
         <p>모두한의원 홈페이지에 방문하신 걸 환영합니다</p>
-      </header>
+      </div>
 
-      <main>
-        <label htmlFor="userId">
-          <input type="text" placeholder="아이디" id="userId" />
-        </label>
+      <div className="loginPageMain">
+        <section className="loginInputBox">
+          <h2 className="sr-only">로그인창</h2>
 
-        <label htmlFor="userPw">
-          <input type="password" placeholder="비밀번호" id="userPw" />
-        </label>
+          <label htmlFor="userId">
+            <input type="text" placeholder="아이디" id="userId" />
+          </label>
 
-        <label htmlFor="remember">
-          <input type="checkbox" id="remember" />
-          아이디 저장
-        </label>
+          <label htmlFor="userPw">
+            <input type="password" placeholder="비밀번호" id="userPw" />
+          </label>
 
-        <button>로그인</button>
+          <label htmlFor="remember" className="remember">
+            <input type="checkbox" id="remember" />
+            아이디 저장
+          </label>
+        </section>
 
-        <section>
-          <h2 className="sr-only">{/* --아이디 비밀번호 찾기 */}</h2>
+        <button className="loginSubmit">로그인</button>
+
+        <section className="findIdPw">
+          <h2 className="sr-only">아이디 비밀번호 찾기</h2>
 
           <a href="#">아이디 찾기</a>
           <span> | </span>
           <a href="#">비밀번호 찾기</a>
         </section>
 
-        <button>회원가입</button>
-      </main>
+        <button className="joinBtn">회원가입</button>
+      </div>
     </>
   );
 }
