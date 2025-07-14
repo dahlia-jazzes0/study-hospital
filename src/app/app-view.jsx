bf9c9ff3d48f2592faf76c1721764336c083ed8f
100gun <xxxxgunshot@gmail.com>
2025-07-14 15:54:27 +0900

    fix: 멤버쉽 버튼 클릭 > 로그인 페이지 이동

--- src/app/app-view.jsx
+++ src/app/app-view.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import { Layout } from './layout.jsx';
 import { HomePage } from '@/pages/home-page.jsx';
 import { AboutPage } from '@/pages/about-page.jsx';
+import { LoginPage } from '@/pages/login-page.jsx';
 
 export function AppView() {
   return (
@@ -9,6 +10,7 @@ export function AppView() {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="login" element={<LoginPage />} />
       </Route>
     </Routes>
   );
