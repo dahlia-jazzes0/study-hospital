b4d55cd3ded9587ee57617e9d95a2de03cea153a
jaeho614 <jaeho614a@gmail.com>
2025-07-15 20:37:49 +0900

    doctorList fetch & skeleton UI 작업 및 config.js 추가

---
+++ src/pages/appointment/config.js
@@ -0,0 +1,11 @@
+export const API_CONFIG = {
+  BASE_URL: 'https://hospital-api.dahlia-jazzes0.workers.dev/api',
+  ENDPOINTS: {
+    DOCTORS: '/doctors/',
+    AVAILABILITY: '/appointments/availability',
+    APPOINTMENTS: '/appointments/',
+  },
+  HEADERS: {
+    'Content-Type': 'application/json',
+  },
+};
