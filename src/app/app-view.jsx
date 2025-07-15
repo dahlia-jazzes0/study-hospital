0e6b9fa239629ce70a1540d9f991e9da2cdbe761
seonghyeon1022 <129020554+seonghyeon1022@users.noreply.github.com>
2025-07-15 18:04:26 +0900

    의료진 소개페이지 구현 (#14)
    * feat: 의료진 소개 페이지 구현
    
    * feat: 네비게이션 Doctors 링크 추가
    
    * NavLink -className 속성 제거
    
    * style: 의료진 소개 페이지 CSS 수정
    
    * style: 의료진 소개 페이지 수정
    
    * style: 의료진 소개 페이지 font-weight 수정
    
    * 의료진 소개 페이지 css 수정 & doctor-card.jsx 리팩토링
    
    ---------
    
    Co-authored-by: seonghyeon.kim <gogo20sh@gmail.com>

--- src/app/app-view.jsx
+++ src/app/app-view.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import { Layout } from './layout.jsx';
 import { HomePage } from '@/pages/home-page.jsx';
 import { AboutPage } from '@/pages/about-page.jsx';
+import { DoctorsPage } from '@/pages/doctors/doctors-page.jsx';
 import { LoginPage } from '@/pages/login-page.jsx';
 import { JoinPage } from '@/pages/join-page.jsx';
 
@@ -11,6 +12,7 @@ export function AppView() {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="doctors" element={<DoctorsPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="join" element={<JoinPage />} />
       </Route>
