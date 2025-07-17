5738bc6783289aa49625daffa573b474094ccecf
hyeon <nguswjd02@gmail.com>
2025-07-17 16:04:09 +0900

    회원가입 step2 flex, grid 갭 조정

--- src/pages/join/components/fields/address-field.jsx
+++ src/pages/join/components/fields/address-field.jsx
@@ -24,50 +24,46 @@ export const AddressField = ({
       >
         {label}
       </label>
-      <div className={styles.formInputWrapper}>
-        <div className={styles.addressInputGroup}>
-          <input
-            type={type}
-            id={id}
-            placeholder={placeholder}
-            aria-label={`${ariaLabel} 필수입력란`}
-            onClick={findAddress}
-            value={value}
-            onChange={onChange}
-            className={`${styles.formInput}`}
-          />
-          <button
-            type="button"
-            id="searchAddressBtn"
-            className={styles.searchAddressBtn}
-            onClick={findAddress}
-          >
-            주소검색
-          </button>
-        </div>
-        <div className={styles.addressDetailGroup}>
-          <input
-            type={type}
-            id={`${id}_main`}
-            placeholder={label}
-            onClick={findAddress}
-            readOnly
-            className={styles.formInput}
-          />
-          <input
-            type={type}
-            id={`${id}_detail`}
-            placeholder="상세주소"
-            aria-label={`${ariaLabel} 상세주소 입력`}
-            className={styles.formInput}
-          />
-        </div>
-        {hasError && (
-          <span className={styles.errorText}>
-            {errorMessage || '필수 입력값입니다.'}
-          </span>
-        )}
+      <div className={styles.formInputGrid}>
+        <input
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          aria-label={`${ariaLabel} 필수입력란`}
+          onClick={findAddress}
+          value={value}
+          onChange={onChange}
+          className={`${styles.formInput}`}
+        />
+        <button
+          type="button"
+          id="searchAddressBtn"
+          className={styles.searchAddressBtn}
+          onClick={findAddress}
+        >
+          주소검색
+        </button>
+        <input
+          type={type}
+          id={`${id}_main`}
+          placeholder={label}
+          onClick={findAddress}
+          readOnly
+          className={styles.formInput}
+        />
+        <input
+          type={type}
+          id={`${id}_detail`}
+          placeholder="상세주소"
+          aria-label={`${ariaLabel} 상세주소 입력`}
+          className={styles.formDetailInput}
+        />
       </div>
+      {hasError && (
+        <span className={styles.errorText}>
+          {errorMessage || '필수 입력값입니다.'}
+        </span>
+      )}
     </div>
   );
 };
