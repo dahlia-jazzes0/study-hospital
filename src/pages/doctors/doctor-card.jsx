789818deb28be42672a95a3dcf360aff29222050
seonghyeon.kim <gogo20sh@gmail.com>
2025-07-22 17:34:17 +0900

    fix: 의료진 소개 페이지 컴포넌트 분할 & isOpen 구조 변경

--- src/pages/doctors/doctor-card.jsx
+++ src/pages/doctors/doctor-card.jsx
@@ -1,121 +1,69 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from '@/pages/doctors/doctor-card.module.css';
 import { DoctorDetail } from './doctor-detail';
 
-export function DoctorCard() {
-  const [doctors, setDoctors] = useState([]);
-  const [departments, setDepartments] = useState([]);
-  const [isOpen, setIsOpen] = useState({});
+export function DoctorCard({ doctor, departmentName }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    const fetchDoctors = async () => {
-      try {
-        const doctorRes = await fetch(
-          'https://hospital-api.dahlia-jazzes0.workers.dev/api/doctors/'
-        );
-        const departmentRes = await fetch(
-          'https://hospital-api.dahlia-jazzes0.workers.dev/api/departments/'
-        );
-
-        if (!doctorRes.ok && !departmentRes.ok) {
-          throw new Error('네트워크 에러');
-        }
-
-        const doctorData = await doctorRes.json();
-        const departmentData = await departmentRes.json();
-
-        setDoctors(doctorData);
-        setDepartments(departmentData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    fetchDoctors();
-  }, []);
-
-  const getDepartmentName = (id) => {
-    const dept = departments.find((d) => d.id === id);
-    return dept ? dept.name : '';
-  };
-
-  const toggleDetail = (id) => {
-    setIsOpen((prev) => ({
-      ...prev,
-      [id]: !prev[id],
-    }));
+  const toggleDetail = () => {
+    setIsOpen((prev) => !prev);
   };
 
   return (
-    <>
-      <h2 className={styles.mainTitle}>의료진소개</h2>
+    <li className={`${styles.doctorCard} ${isOpen ? styles.open : ''}`}>
+      <img
+        src={`${import.meta.env.BASE_URL}images/doctor-${doctor.id}.png`}
+        alt={`${doctor.name} 이미지`}
+        className={styles.doctorImage}
+      />
 
-      <ul className={styles.doctorList}>
-        {doctors.map((doctor) => (
-          <li
-            key={doctor.id}
-            className={`${styles.doctorCard} ${isOpen[doctor.id] ? styles.open : ''}`}
-          >
-            <img
-              src={`${import.meta.env.BASE_URL}images/doctor-${doctor.id}.png`}
-              alt={`${doctor.name} 이미지`}
-              className={styles.doctorImage}
-            />
+      <section className={styles.doctorInfo}>
+        <h3 className={styles.doctorName}>
+          {doctor.name} {doctor.chief && '대표'}원장 ({departmentName})
+        </h3>
 
-            <section className={styles.doctorInfo}>
-              <h3 className={styles.doctorName}>
-                {doctor.name} {doctor.chief && '대표'}원장 (
-                {getDepartmentName(doctor.departmentId)})
-              </h3>
+        <dl className={styles.doctorDetails}>
+          <div className={styles.detailItem}>
+            <dt className={styles.detailLabel}>학력</dt>
+            <dd className={styles.detailValue}>{doctor.education}</dd>
+          </div>
 
-              <dl className={styles.doctorDetails}>
-                <div className={styles.detailItem}>
-                  <dt className={styles.detailLabel}>학력</dt>
-                  <dd className={styles.detailValue}>{doctor.education}</dd>
-                </div>
+          <div className={styles.detailItem}>
+            <dt className={styles.detailLabel}>경력</dt>
+            {doctor.experience.map((item, i) => (
+              <dd key={i} className={styles.detailValue}>
+                {item}
+              </dd>
+            ))}
+          </div>
 
-                <div className={styles.detailItem}>
-                  <dt className={styles.detailLabel}>경력</dt>
-                  {doctor.experience.map((item, i) => (
-                    <dd key={i} className={styles.detailValue}>
-                      {item}
-                    </dd>
-                  ))}
-                </div>
+          <div className={styles.detailItem}>
+            <dt className={styles.detailLabel}>자격 및 인증</dt>
+            {doctor.certifications.map((item, i) => (
+              <dd key={i} className={styles.detailValue}>
+                {item}
+              </dd>
+            ))}
+          </div>
 
-                <div className={styles.detailItem}>
-                  <dt className={styles.detailLabel}>자격 및 인증</dt>
-                  {doctor.certifications.map((item, i) => (
-                    <dd key={i} className={styles.detailValue}>
-                      {item}
-                    </dd>
-                  ))}
-                </div>
-
-                <div className={styles.detailItem}>
-                  <dt className={styles.detailLabel}>학회 활동</dt>
-                  {doctor.scholarActivities.map((item, i) => (
-                    <dd key={i} className={styles.detailValue}>
-                      {item}
-                    </dd>
-                  ))}
-                </div>
-              </dl>
-              <button
-                className={styles.toggleButton}
-                onClick={() => toggleDetail(doctor.id)}
-              >
-                <img
-                  src={`${import.meta.env.BASE_URL}images/arrow.png`}
-                  alt="의료진 상세 정보 버튼"
-                  className={styles.arrowIcon}
-                />
-              </button>
-              {isOpen[doctor.id] && <DoctorDetail doctor={doctor} />}
-            </section>
-          </li>
-        ))}
-      </ul>
-    </>
+          <div className={styles.detailItem}>
+            <dt className={styles.detailLabel}>학회 활동</dt>
+            {doctor.scholarActivities.map((item, i) => (
+              <dd key={i} className={styles.detailValue}>
+                {item}
+              </dd>
+            ))}
+          </div>
+        </dl>
+        <button className={styles.toggleButton} onClick={toggleDetail}>
+          <img
+            src={`${import.meta.env.BASE_URL}images/arrow.png`}
+            alt="의료진 상세 정보 버튼"
+            className={styles.arrowIcon}
+          />
+        </button>
+        {isOpen && <DoctorDetail doctor={doctor} />}
+      </section>
+    </li>
   );
 }
