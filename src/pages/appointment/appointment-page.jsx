81d20fb6e1940f8f32687eabb62aaa94e3c19f04
jaeho614 <jaeho614a@gmail.com>
2025-07-22 09:49:44 +0900

    appointment-page wrapper 추가 및 CSS 수정, description button 수정

--- src/pages/appointment/appointment-page.jsx
+++ src/pages/appointment/appointment-page.jsx
@@ -42,49 +42,51 @@ export function AppointmentPage() {
   const { deleteAppointment } = useDeleteAppointment();
 
   return (
-    <main className={styles.appointment}>
-      <h1>진료예약</h1>
+    <div className={styles.appointmentWrapper}>
+      <div className={styles.appointment}>
+        <h1>진료예약</h1>
 
-      <DoctorList
-        doctors={doctors}
-        isLoading={isLoading}
-        selectedDoctorId={appointmentData.doctor.id}
-        onDoctorSelect={handleDoctorSelect}
-      />
+        <DoctorList
+          doctors={doctors}
+          isLoading={isLoading}
+          selectedDoctorId={appointmentData.doctor.id}
+          onDoctorSelect={handleDoctorSelect}
+        />
 
-      <AppointmentCalendar
-        timeTable={timeTable}
-        appointmentData={appointmentData}
-        isTimeTableLoading={isTimeTableLoading}
-        onDateSelect={handleDateSelect}
-        onTimeSelect={handleTimeSelect}
-        isAppointmentComplete={isAppointmentComplete}
-        handleNavigationChange={handleNavigationChange}
-        holidays={holidays}
-        isHolidayDate={isHolidayDate}
-        isWeekend={isWeekend}
-        isDisabledDate={isDisabledDate}
-        handleCloseConfirmModal={handleCloseConfirmModal}
-        handleCloseCancleModal={handleCloseCancleModal}
-        myAppointments={myAppointments}
-      />
-      {showConfirmModal && (
-        <ConfirmModal
+        <AppointmentCalendar
+          timeTable={timeTable}
           appointmentData={appointmentData}
-          getMyAppointment={getMyAppointment}
-          resetAppointment={resetAppointment}
-          submitAppointment={submitAppointment}
-          onClose={handleCloseConfirmModal}
-        />
-      )}
-      {showCancleModal && (
-        <CancleModal
-          getMyAppointment={getMyAppointment}
+          isTimeTableLoading={isTimeTableLoading}
+          onDateSelect={handleDateSelect}
+          onTimeSelect={handleTimeSelect}
+          isAppointmentComplete={isAppointmentComplete}
+          handleNavigationChange={handleNavigationChange}
+          holidays={holidays}
+          isHolidayDate={isHolidayDate}
+          isWeekend={isWeekend}
+          isDisabledDate={isDisabledDate}
+          handleCloseConfirmModal={handleCloseConfirmModal}
+          handleCloseCancleModal={handleCloseCancleModal}
           myAppointments={myAppointments}
-          deleteAppointment={deleteAppointment}
-          onClose={handleCloseCancleModal}
         />
-      )}
-    </main>
+        {showConfirmModal && (
+          <ConfirmModal
+            appointmentData={appointmentData}
+            getMyAppointment={getMyAppointment}
+            resetAppointment={resetAppointment}
+            submitAppointment={submitAppointment}
+            onClose={handleCloseConfirmModal}
+          />
+        )}
+        {showCancleModal && (
+          <CancleModal
+            getMyAppointment={getMyAppointment}
+            myAppointments={myAppointments}
+            deleteAppointment={deleteAppointment}
+            onClose={handleCloseCancleModal}
+          />
+        )}
+      </div>
+    </div>
   );
 }
